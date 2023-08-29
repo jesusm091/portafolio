1d77247c260579ef786d431c284c00bd64b8cf2c
manuel <jesus.developer.3000@outlook.es>
2023-08-28 23:51:47 -0400

    :tada: init proyect

---
+++ assets/js/index.js
@@ -0,0 +1,11 @@
+// @codekit-prepend "/vendor/hammer-2.0.8.js";
+var arr = [];
+$(document).ready(function() {
+
+    arr.push({ title: 'Aplicacion Movil YO VOY', descipcion: '', link: '' });
+    arr.push({ title: 'Pagina web katas ', descipcion: '', link: '' });
+    arr.push({ title: 'Pagina web Monomedia', descipcion: '', link: '' });
+    console.log(arr)
+
+
+});
